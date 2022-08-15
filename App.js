c9778c8bef2ae688f6c9e1b272d7791bf2c0909d
harshk262001 <111220696+harshk262001@users.noreply.github.com>
2022-08-15 20:31:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import Loginform from './components/Loginform';
+
+function App() {
+    return ( 
+    <div className = "page">
+        <Loginform />
+
+        </div>
+    );
+}
+
+export default App;
